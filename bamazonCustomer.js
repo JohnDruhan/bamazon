b6b2b5b9927fdce99426b1baa0f7e47f056a3d82
JohnDruhan <jfrancisdruhan@comcast.net>
2019-03-11 14:04:22 -0400

    store setup

---
+++ bamazonCustomer.js
@@ -0,0 +1,120 @@
+const cTable = require('console.table');
+const inquirer = require('inquirer');
+const connection = require('./database');
+
+connection.connect(function (err) {
+    if (err) console.log("Error: " + err);
+    console.log(`Connected as ID ${connection.threadId}\n`);
+    displayProducts();
+});
+
+// Display products from database (id, name, price)
+function displayProducts() {
+    connection.query(`
+    SELECT item_id, product_name, price 
+    FROM products 
+    WHERE stock_quantity > 0`, (err, response) => {
+            if (err) console.log(err);
+            response.forEach(p => p.price = `${p.price.toFixed(2)}`);
+            console.table(response);
+            // connection.end();
+            start();
+        });
+}
+
+// Start app
+function start() {
+
+    inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'id',
+                message: 'Please enter the ID of the item you would like to purchase.'
+            },
+            {
+                type: 'input',
+                name: 'quantity',
+                message: 'How many would you like to purchase?'
+            }
+        ]).then(function (answers) {
+            getProduct(answers.id, function (err, products) {
+                if (err) console.log(err);
+                products.forEach(p => p.price = `${p.price.toFixed(2)}`);
+                console.table(products);
+
+                const stock = products[0].stock_quantity;
+                const price = products[0].price;
+
+                // If insufficient inventory, prevent order from processing
+                if (stock < answers.quantity) {
+                    console.log(`Sorry, only ${stock} remain`);
+                    connection.end();
+
+                    // If sufficient inventory, fulfill order
+                } else {
+                    // Update quantity in SQL database
+                    const newStock = stock - answers.quantity;
+                    updateStockQuantity(answers.id, newStock, function (err) {
+                        if (err) console.log(err);
+                        // Once updated, display total cost of purchase 
+                        // TODO: If only purchasing one of an item, returns price as "$NAN"
+                        console.log(`The total for ${answers.quantity} ${products[0].product_name} is ${price * answers.quantity}`);
+                        connection.end();
+
+                        // Confirm purchase
+                        inquirer
+                            .prompt([
+                                {
+                                    type: 'confirm',
+                                    name: 'confirm',
+                                    message: 'Are you sure?',
+                                }
+                            ]).then(function (answer) {
+                                if (answer.confirm === true) {
+                                    console.log(`Bamazon Primo will deliver your ${products[0].product_name} in two business days!`);
+                                } else {
+                                    console.log(`We're sorry you changed your mind.`);
+                                }
+                            })
+                    });
+                }
+            });
+        });
+}
+
+function updateStockQuantity(itemId, quantity, callback) {
+    connection.query(`
+    UPDATE products 
+    SET stock_quantity = ${quantity} 
+    WHERE item_id = ${itemId}
+    `, callback);
+}
+
+function getStockQuantity(itemId, callback) {
+    connection.query(`
+    SELECT stock_quantity
+    FROM products
+    WHERE item_id = ${itemId}
+    `, callback);
+}
+
+function getProduct(itemId, callback) {
+    connection.query(`
+    SELECT *
+    FROM products
+    WHERE item_id = ${itemId}
+    `, callback);
+}
+
+function formatTable(products) {
+    console.table([
+        {
+            id: products.item_id,
+            name: products.product_name,
+            department: products.department_name,
+            price: products.price,
+            stock: products.stock_quantity
+        }
+    ]);
+}
---
+++ bamazonManager.js
@@ -0,0 +1,171 @@
+const cTable = require('console.table');
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const chalk = require('chalk');
+const Product = require('./product.js');
+const connection = require('./database');
+
+connection.connect(function (err) {
+    if (err) console.log(chalk.bgRed(err));
+    console.log(chalk.gray(`Connected as ID ${connection.threadId}\n`));
+    menu();
+});
+
+// menu options
+function menu() {
+    console.log('......................................................................')
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                name: 'menu',
+                message: 'Welcome! Please select from the following options: ',
+                choices: ['View Available Products', 'View Low Inventory', 'Add to Inventory', 'Add New Product', 'Exit']
+            }
+        ]).then(function (response) {
+
+            if (response.menu === 'View Available Products') {
+                // List available items (item IDs, names, prices, quantities)
+                displayAll(function (err, products) {
+                    if (err) console.log(chalk.bgRed(err));
+                    products.forEach(p => p.price = `$ ${p.price.toFixed(2)}`);
+                    console.table(products);
+                    menu();
+                });
+
+            } else if (response.menu === 'View Low Inventory') {
+                // List all items where inventory < 5
+                displayLowInventory(function (err, products) {
+                    if (err) console.log(chalk.bgRed(err));
+
+                    if (products.length === 0) {
+                        console.log('There are currently no items with low inventory.')
+                    } else {
+                        console.table(products);
+                    }
+                    menu();
+                });
+
+            } else if (response.menu === 'Add to Inventory') {
+                // Display prompt to let manager "add more" of any item currently in the store
+                inquirer
+                    .prompt([
+                        {
+                            type: 'input',
+                            name: 'itemId',
+                            message: 'Please enter the ID of the item you would like to restock.',
+                        },
+                        {
+                            type: 'input',
+                            name: 'quantity',
+                            message: 'How many would you like to add to the inventory?'
+                        }
+                    ]).then(function (answers) {
+                        // Check if itemId exists in the database
+                        // if(answers.itemId < 0 || answers.itemId > products.length) {
+                        //     console.log('Please enter a valid ID.');
+                        //     menu();
+                        // } else {
+                        // Update database 
+                        updateStockQuantity(answers.itemId, answers.quantity, function (err) {
+                            if (err) console.log(chalk.bgRed(err));
+                            console.log(`Item ${answers.itemId} successfully restocked (${answers.quantity} added).`);
+                            displayUpdatedProduct(answers.itemId, function () {
+                                menu();
+                            });
+                        });
+                        // }
+                    });
+
+            } else if (response.menu === 'Add New Product') {
+                // Allow manager to add completely new product to store
+                console.log('Please follow the prompt to add information about the new item.');
+                inquirer
+                    .prompt([
+                        {
+                            type: 'input',
+                            name: 'newName',
+                            message: 'What is the name of the item you would like to add?'
+                        },
+                        {
+                            type: 'input',
+                            name: 'newDepartment',
+                            message: 'What is the name of the department?'
+                        },
+                        {
+                            type: 'input',
+                            name: 'newPrice',
+                            message: 'What is the price per item?'
+                        },
+                        {
+                            type: 'input',
+                            name: 'newQuantity',
+                            message: 'How many would you like to add?'
+                        }
+                    ]).then(function (answers) {
+                        addProduct(answers.newName, answers.newDepartment, answers.newPrice, answers.newQuantity, function (err, response) {
+                            if (err) console.log(chalk.bgRed(err));
+                            displayAll(function (err, products) {
+                                if (err) console.log(chalk.bgRed(err));
+                                products.forEach(p => p.price = `$ ${p.price.toFixed(2)}`);
+                                console.table(products);
+                                console.log('\nItem successfully added!');
+                                menu();
+                            });
+                        });
+                    });
+
+            } else {
+                console.log('You have exited the program.');
+                connection.end();
+            }
+        });
+}
+
+function displayAll(callback) {
+    connection.query(`
+    SELECT item_id, product_name, price, stock_quantity 
+    FROM products
+    `, callback);
+}
+
+function displayLowInventory(callback) {
+    connection.query(`
+    SELECT item_id, product_name, stock_quantity
+    FROM products
+    WHERE stock_quantity < 5
+    `, callback);
+}
+
+function updateStockQuantity(itemId, quantity, callback) {
+    connection.query(`
+    UPDATE products 
+    SET stock_quantity = stock_quantity + ${quantity} 
+    WHERE item_id = ${itemId}
+    `, callback);
+}
+
+function displayUpdatedProduct(itemId, callback) {
+    connection.query(`
+    SELECT item_id, product_name, price, inventory
+    FROM products
+    WHERE item_id = ${itemId}`, callback);
+}
+
+function addProduct(name, department, price, quantity, callback) {
+    connection.query(`
+    INSERT INTO products(product_name, department_name, price, stock_quantity)
+    VALUES('${name}', '${department}', ${price}, ${quantity})`, callback);
+}
+
+function formatTable(products) {
+    console.table([
+        {
+            id: products.item_id,
+            name: products.product_name,
+            department: products.department_name,
+            price: products.price,
+            stock: products.stock_quantity
+        }
+    ]);
+}
